a2b3745dde15060a0b9ed907250961f890e12a05
unknown <shwetha123panamps@gmail.com>
2020-09-14 23:13:32 +0530

    Server side backend completed

--- src/App.js
+++ src/App.js
@@ -82,7 +82,26 @@ class App extends Component {
   onButtonSubmit=(event) =>{
     this.setState({imageUrl: this.state.input});
     app.models.predict( Clarifai.FACE_DETECT_MODEL,this.state.input)
-    .then(response=>this.displayFaceBox(this.calculateFaceLocation(response))) 
+    .then(response=>{
+      if(response){
+        fetch('http://localhost:3000/image',{
+           method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        id: this.state.user.id
+        
+      })
+        })
+         .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+       
+       
+      }
+         this.displayFaceBox(this.calculateFaceLocation(response))
+    }
+    ) 
       .catch(err=>console.log(err));
 
       // do something with response
@@ -120,8 +139,8 @@ class App extends Component {
       <FaceDetect box={box} imageUrl={imageUrl}/>
       </div> 
        :(
-        route==='signin'?
-         <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+        route==='signin'
+         ?<Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
          :<Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
        )
      
