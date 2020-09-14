d42fc791b9db8d94003d127393bd855e50fd738a
unknown <shwetha123panamps@gmail.com>
2020-09-14 17:33:12 +0530

    Adding Backend Server

--- src/App.js
+++ src/App.js
@@ -33,9 +33,31 @@ class App extends Component {
       imageUrl:'',
       box:{},
       route:'signin',
-      isSignedIn:false
+      isSignedIn:false,
+      user:{
+         id:'',
+    name:'',
+    email:'',
+   
+    
+    entries:0,
+    joined:''
+      }
     }
   }
+  loadUser=(data)=>{
+    this.setState({user:{
+        id:data.id,
+    name:data.name,
+    email:data.email,
+   
+    
+    entries:data.entries,
+    joined:data.joined
+
+    }})
+  }
+   
   calculateFaceLocation=(data)=>{
    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
@@ -89,7 +111,7 @@ class App extends Component {
       {route==='home'
        ?<div>
       <Logo/>
-       <Rank/>
+      <Rank name={this.state.user.name} entries={this.state.user.entries}/>
        <ImageLinkForm onInputChange={this.onInputChange}
            onButtonSubmit={this.onButtonSubmit}/>
       
@@ -99,8 +121,8 @@ class App extends Component {
       </div> 
        :(
         route==='signin'?
-         <Signin onRouteChange={this.onRouteChange}/>
-         :<Register onRouteChange={this.onRouteChange}/>
+         <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+         :<Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
        )
      
       
--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const Rank=()=>{
+const Rank=({name,entries})=>{
 	return(
 		<div>
-		<div className='white f3'>
-		   {'Shwetha,Your rank is'}
-	    </div>
-	    <div className='white f1'>
-		   {'#5'}
-		   </div>
+		{`${name} , your current rank is...`}
+      <div className='white f1 '>
+        {entries}
+      </div>
 		   </div>
 
 		
